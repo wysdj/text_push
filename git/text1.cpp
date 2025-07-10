6c1aea951b319d601be86642d7b6e146019df3c0
yuanzuquan <13459990782@qq.com>
2025-07-10 11:32:38 +0800

    这是一次pr吗？

--- git/text1.cpp
+++ git/text1.cpp
@@ -7,5 +7,7 @@ int main() {
 	cout<< "Hello, World!" << endl;
 	cout << "This is a simple C++ program." << endl;
 	cout << "asdfasdfasf";
+	cout << "this is a branch new text<<end";
+	//a/sdf/asdf
 	return 0;
 }
